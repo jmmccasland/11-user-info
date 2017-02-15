4a66b5ec151e3bbd4891226b48834304c47fdd34
John Michael McCasland <jmmccasland@gmail.com>
2017-02-15 15:46:22 -0600

    JS connected

--- app/index.js
+++ app/index.js
@@ -1,2 +1,10 @@
 import 'whatwg-fetch';
-import 'data';
+import data from './data';
+
+
+// const userPhotoEl = document.querySelector('.hero__user-photo');
+// userPhotoEl.src = results.picture.thumbnail;
+
+debugger;
+const userNameEl = document.querySelector('.user-info__name');
+userNameEl.innerText = data.results.email;

9356ba097436511b31e2c8140803deb1a93ad2fd
John Michael McCasland <jmmccasland@gmail.com>
2017-02-15 16:23:38 -0600

    Project Complete

--- app/index.js
+++ app/index.js
@@ -1,10 +1,22 @@
 import 'whatwg-fetch';
 import data from './data';
 
+// user photo
+const userPhotoEl = document.querySelector('.hero__user-photo');
+userPhotoEl.src = data.results[0].picture.large;
 
-// const userPhotoEl = document.querySelector('.hero__user-photo');
-// userPhotoEl.src = results.picture.thumbnail;
-
-debugger;
+// user name
 const userNameEl = document.querySelector('.user-info__name');
-userNameEl.innerText = data.results.email;
+userNameEl.innerText = `${data.results[0].name.first} ${data.results[0].name.last}`;
+
+// user email
+const userEmailEl = document.querySelector('.user-info__email');
+userEmailEl.innerText = data.results[0].email;
+
+// user cell phone number
+const userPhoneEl = document.querySelector('.user-info__phone');
+userPhoneEl.innerText = data.results[0].cell;
+
+// user location
+const userLocationEl = document.querySelector('.user-info__location');
+userLocationEl.innerText = `${data.results[0].location.city}, ${data.results[0].location.state}`;
